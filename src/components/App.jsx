999175bed4583c825a1df64ba83ca84364e07d93
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-12-31 04:04:05 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from './profile/user.json';
+import Profile from './profile/Profile';
+import data from './statistics/data.json';
+import Statistics from './statistics/Statistics';
+import friends from './friendList/friends.json';
+import FriendList from './friendList/FriendList';
+import transactions from './transactionHistory/transactions.json';
+import TransactionHistory from './transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+    </>
   );
 };
+export default App;
